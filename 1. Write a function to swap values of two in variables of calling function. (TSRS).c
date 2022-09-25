9388fd5c82df9e477ab13ddc404e8a45bc95d018
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2022-09-25 10:02:58 +0530

    Create 1. Write a function to swap values of two in variables of calling function. (TSRS).c

---
+++ 1. Write a function to swap values of two in variables of calling function. (TSRS).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int swap(int *, int *);
+int main()
+{
+    int a, b;
+    printf("enter 2 numbers\n");
+    scanf("%d%d", &a, &b);
+    swap(&a, &b);
+    printf("after swapping\n");
+    printf("a = %d\tb = %d", a, b);
+    return 0;
+}
+int swap(int *a, int *b)
+{
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+output:-
+  enter 2 numbers
+5
+6
+after swapping
+a = 6	b = 5
